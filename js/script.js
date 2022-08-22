2c827c15f2460fdda8c62520e2ed4603fb283010
faykarr <nasyathfaykar@gmail.com>
2022-08-23 00:51:43 +0700

    Finishing slicing all components & images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menuList = document.querySelector('.menu-list');
 const btnNav = document.querySelector('.btn-nav');
-// const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.nav-section');
 
 // * Hamburger Menu for Mobile Devices
 hamburger.addEventListener('click', () => {
@@ -10,11 +10,11 @@ hamburger.addEventListener('click', () => {
     btnNav.classList.toggle('btn-nav-active');
 });
 
-// // * Add shadows to navbar when scrolling
-// window.onscroll = function () {
-//     if (window.pageYOffset > 0) {
-//         navbar.style.boxShadow = 'rgba(0, 0, 0, 0.85) 0px 1px 4px';
-//     } else {
-//         navbar.style.boxShadow = 'none';
-//     }
-// }
+// * Add shadows to navbar when scrolling
+window.onscroll = function () {
+    if (window.pageYOffset > 0) {
+        navbar.style.boxShadow = 'rgba(0, 0, 0, 0.85) 0px 1px 4px';
+    } else {
+        navbar.style.boxShadow = 'none';
+    }
+}
