5c090a70c583410b0b1749ae881d2b740fecad34
faykarr <nasyathfaykar@gmail.com>
2022-08-30 20:56:11 +0700

    Fixing Send WhatsApp & Finishing all components

--- js/script.js
+++ js/script.js
@@ -18,3 +18,14 @@ window.onscroll = function () {
         navbar.style.boxShadow = 'none';
     }
 }
+
+// * Send Message to WhatsApp
+function send_message() {
+    let nim = document.getElementById('nim').value;
+    let name = document.getElementById('nama').value;
+    let kelas = document.getElementById('kelas').value;
+    let noWA = document.getElementById('noWA').value;
+    let alamat = document.getElementById('alamat').value;
+    var send = window.open('https://api.whatsapp.com/send?phone=6288806923500&text=Hai+kak%2C+saya+ingin+mendaftar+sebagai+bagian+dari+UPT+Komputer.%0D%0ABerikut+data-data+yang+saya+lampirkan+%3A%0D%0A1.+NIM+%3A%0D%0A'+nim+'2.+Nama+Lengkap+%3A%0D%0A'+name+'3.+Kode+Kelas+%3A%0D%0A'+kelas+'4.+No+WA+%3A%0D%0A'+noWA+'5.+Alamat+%3A'+alamat,'_blank');
+    send.focus();
+}
